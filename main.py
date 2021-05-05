a27039594583eaf197886ab7a3e62a4dd00a17af
Brent <brent.gerets@student.uhasselt.be>
2021-05-05 19:36:07 +0200

    removed debugging code

--- main.py
+++
@@ -1,8 +0,0 @@
-from py3dbp.main import *
-
-if __name__ == '__main__':
-    bin = Bin("1", 100, 100, 100)
-    item1 = Item("1", 100, 100, 10, False)
-    item2 = Item("2", 10, 100, 100, False)
-    bins = pack(100, 100, 100, [item1, item2])
-    print(bins)
